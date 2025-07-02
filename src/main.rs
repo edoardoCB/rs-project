7ecf5baf2a058588940ead968405e96365ec00be
Edoardo Coronado <eecb76@hotmail.com>
2025-07-02 23:08:22 +0200

    feat: adds greet function binded to 127.0.0.1:8080

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name: &str = req.match_info().get("name").unwrap_or("World");
+    HttpResponse::Ok().body(format!("Hello, {}!", &name))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>{
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8080").expect("Can not bind to 127.0.0.1:8080")
+        .run()
+        .await
 }
