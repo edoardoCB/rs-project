90ce5bbd05111b906bd82db1b3176b7bd3d8d9a2
Edoardo Coronado <eecb76@hotmail.com>
2025-07-03 22:28:53 +0200

    feat: uses main app and endpoints from library

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 
-use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name: &str = req.match_info().get("name").unwrap_or("World");
-    HttpResponse::Ok().body(format!("Hello, {}!", &name))
-}
+use rs_project::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error>{
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-        .bind("127.0.0.1:8080").expect("Can not bind to 127.0.0.1:8080")
-        .run()
-        .await
+    run().await
 }
